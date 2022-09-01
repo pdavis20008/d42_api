74117bf930a87cc7ad00d4d44c161b659a55a864
pdavis20008 <64021536+pdavis20008@users.noreply.github.com>
2022-09-01 10:09:09 -0400

    Update d42_api.py

--- d42_api.py
+++ d42_api.py
@@ -1,6 +1,9 @@
 import urllib3
 import requests
 import base64
+from pprint import pprint
+from getpass import getpass
+import sys
 urllib3.disable_warnings()
 
 
@@ -9,7 +12,7 @@ class d42_api:
         self.ipaddress = ipaddress
         self.urluser = urluser
         self.urlpassword = urlpass
-        self.base_url = f"https://{ipaddress}/api/1.0/"
+        self.base_url = f"https://{ipaddress}/api/"
         self.headers = {"Authorization": f"Basic {str(base64.b64encode(bytes(urluser+':'+urlpass,encoding='utf-8')),encoding='utf-8')}"}
 
 
@@ -25,15 +28,45 @@ class d42_api:
         req = requests.put(f'{self.base_url}{url_path}',verify=False,headers=self.headers,params=params,data=data)
         return req.status_code
 
+    def delete(self,url_path):
+        req = requests.delete(f'{self.base_url}{url_path}',verify=False,headers=self.headers)
+        return req.status_code
+
     def findIp(self,ipaddress):
-        results = self.get(url_path='search/',params=f"query=ip&string={ipaddress}")
+        results = self.get(url_path='1.0/search/',params=f"query=ip&string={ipaddress}")
         return results.json()['ips']
 
-    def assignIp(self,ipaddres):
+    def assignIp(self,ipaddress):
         return 'Method not complete'
 
+    def unassignIp(self,ipaddress,type):
+        data = {
+            "ipaddress": ipaddress,
+            "available": "yes",
+            "label": "",
+            "mac_address":"",
+            "device":"",
+            "type":type
+        }
+        results = self.post(url_path='1.0/ips/',data=data)
+        if results.status_code == 200:
+            return f'{ipaddress} now set to available'
+        else:
+            return f'{ipaddress}: Something went wrong. Error {results.status_code}'
+
+    def findDevice(self,devname):
+        results = self.get(url_path=f'1.0/devices/name/{devname}')
+        return results.json()
+
+    def deleteDevice(self,devid):
+        result = self.delete(url_path=f'1.0/devices/{devid}/')
+        if result == 200:
+            return f"{devid}: Successfully deleted"
+        else:
+            return f"{devid}: Something went wrong. Error {result}"
+
     def listAutoDiscovery(self,type='pingsweep'):
-        results = self.get(f'auto_discovery/{type}')
+        results = self.get(f'1.0/auto_discovery/{type}')
         return results.json()['jobs']
 
     def getHealthStats(self):
